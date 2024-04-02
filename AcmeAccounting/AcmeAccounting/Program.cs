f6b763374cb3c8a3fc1acb7f6f286ed3f4f85df8
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-02 19:31:21 -0400

    AcmeAccounting
    Created program that prints a string on the console and uses escape character

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace AcmeAccounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
