37fe6ecee7d7e482f0c40d3d47e314dcc6d8c0f7
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-03 19:35:38 -0400

    MathChallenge
    basic math operations and added comments

---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 9;
+            int num2 = 8;
+            // adding num1 and num2
+            int addTwo = num1 + num2;
+            Console.WriteLine(addTwo);
+
+            // Subtracting num1 and num2
+            int minusTwo = num1 - num2;
+            Console.WriteLine(minusTwo);
+
+            // Multiply num1 and num2
+            int timesTwo = num1 * num2;
+            Console.WriteLine(timesTwo);
+
+            // Divide num1 and num2
+            int divTwo = num1 / num2;
+            Console.WriteLine(divTwo);
+
+            Console.ReadLine();
+        }
+    }
+}
