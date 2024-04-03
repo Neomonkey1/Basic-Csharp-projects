e7849b8b68b57f715f22e1a9f80242d5362ffbc5
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-02 20:57:42 -0400

    VariablesAndDataTypes
    showing different Variables and Data types also showing how to comment in C# and how to do casting

---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is how you comment on C# . Also you can highlight your code and hit "CTRL + K + C" to add // to highlighted code
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+            // You can highlight your code and hit "CTRL + K + U" to remove the // from highlighted code
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber); // casting done here to convert string favoriteNumber to int favNum
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemperature = -23;
+            char questionMark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202092;
+
+            float secondsLeft = 2.62f;
+
+            short temperaturOnMars = -341;
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+        }
+    }
+}
