c3bc69c134c157b516f4cdc26bc7dbd92dc68da5
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-03 20:01:01 -0400

    MathAndComparisonOperators
    Basic Math and Comparisons

---
+++ MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine("Five plus Ten = " + total.ToString());
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five = " + difference);
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemperature = 70;
+            int currentTemperature = 70;
+
+            //bool isWarm = currentTemperature >= roomTemperature;
+            bool isWarm = currentTemperature != roomTemperature;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+
+
+        }
+    }
+}
