7ed1aa318e157d95c95bc6e4d4e6d644afc2dc06
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-04 19:57:20 -0400

    Conditional Statements
    basic use of if, else if and else statements added comments

---
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // String variable
+            string name = "Ray";
+
+            // using if, else if and else statements to check for several conditions
+            if (name == "Jesse")
+            {
+                Console.WriteLine("Your name is Jesse");
+            }
+            else if (name == "Brett")
+            {
+                Console.WriteLine("Your name is Brett");
+            }
+            else
+            {
+                Console.WriteLine("Your name is not Jesse or Brett");
+            }
+            Console.ReadLine();
+        }
+    }
+}
