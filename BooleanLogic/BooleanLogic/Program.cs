866d799d5615a54cbe39e4a3712fdb6f78c442b8
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-03 21:40:56 -0400

    boolean logic
    Basic boolean logic shown

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // AND operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // OR operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            // equal operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            // not equal operator
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // XOR operator
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+            // more than one operator can be used together
+            Console.WriteLine(true && true && true && false);
+            Console.WriteLine(true && true && true || false);
+            
+            Console.ReadLine();
+        }
+    }
+}
