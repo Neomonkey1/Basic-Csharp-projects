0ea22c2e5b7499c7ca23b5761d3d9980c6de19ac
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-05 19:26:49 -0400

    ConditionalStatement
    Added switch statement and added comments

--- ConditionalStatements/ConditionalStatements/Program.cs
+++ ConditionalStatements/ConditionalStatements/Program.cs
@@ -44,6 +44,39 @@ namespace ConditionalStatements
             string result = num1 > num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
             Console.WriteLine(result);
 
+            //  Using switch statement, code will go through each 'case' till it gets to the right one then break out of the program,
+            //  If nothing is picked switch statement will go with default.
+            int day = 5;
+            switch(day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Looking forward to the Weekend.");
+                    break;
+
+            }
+
+
             Console.ReadLine();
         }
     }
