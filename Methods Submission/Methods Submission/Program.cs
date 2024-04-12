29d7fd20362416205c2ae6987efcf40503260aff
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-11 21:11:43 -0400

    Methods Submission
    Created a program, also created a class, in the class made three methods, asked user for input, convert input to int, used input with class methods to get results, printed results on console. added comments

---
+++ Methods Submission/Methods Submission/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Asking the user for input convert input to int variable.
+            Console.WriteLine("Pick a number to perform operations on.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            MathOperations mathOperations1 = new MathOperations();
+            int result1 = mathOperations1.AddNumber(userNum);
+            int result2 = mathOperations1.SubtractNumber(userNum);
+            int result3 = mathOperations1.MultiplyNumber(userNum);
+
+            Console.WriteLine($"you entered {userNum} we added 10 to equal {result1}");
+            Console.WriteLine($"you entered {userNum} we subtract 5 to equal {result2}");
+            Console.WriteLine($"you entered {userNum} we multiply by 2 to equal {result3}");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods Submission/Methods Submission/mathOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Submission
+{
+    //  Made public class MathOperations with three methods
+    public class MathOperations
+    {
+        //  Made first public Method adding method
+        public int AddNumber (int num)
+        {
+            return num + 10;
+        }
+
+        // Made second public Method subtracting method
+        public int SubtractNumber (int num)
+        {
+            return num - 5;
+        }
+
+        //  Made third public Method multiply method
+        public int MultiplyNumber (int num)
+        {
+            return num * 2;
+        }
+    }
+}
