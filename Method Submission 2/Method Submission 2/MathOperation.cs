aa384560f2cd36c5f1219002c2b68a68d128c8b8
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-11 22:46:52 -0400

    Method Submission 2
    Creating a program. Made a new class and added a method to it. printed results to console

---
+++ Method Submission 2/Method Submission 2/MathOperation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_2
+{
+    public class MathOperation
+    {
+        //  Making public method for adding
+        public int AddNumbers (int num)
+        {
+            return num + 25;
+        }
+    }
+}
---
+++ Method Submission 2/Method Submission 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperation math1 = new MathOperation();
+            int results1= math1.AddNumbers(15);
+
+
+            Console.WriteLine(results1);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
