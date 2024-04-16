f70a37f9174fcc3625db5cd359f804acbaf71956
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-16 19:57:35 -0400

    Update

--- TwentyOne/TwentyOne/Game.cs
+++ TwentyOne/TwentyOne/Game.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace TwentyOne
 {
-    public class Game
+    public abstract class Game
     {
         public List<string> Players { get; set; }
         public string Name { get; set; }
         public string Dealer { get; set; }
 
-        public void ListPlayers()
+        public abstract void Play();
+        public virtual void ListPlayers()
         {
             foreach (string player in Players)
             {
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,18 +10,22 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            game.ListPlayers();
             Console.ReadLine();
+
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
+
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+
+            //Console.WriteLine(deck.Cards.Count);
+            //Console.ReadLine();
         }
 
        
