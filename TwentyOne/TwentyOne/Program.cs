90c6d43149201d248f43462f8da4396cb4500be8
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-15 22:36:34 -0400

    TwentyOne
    update

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,24 +10,18 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>()
-            { "Jesse", "Bill", "Joe" };
-            game.ListPlayers();
-            game.Play();
-            Console.ReadLine();
 
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
-           
-            
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            
-            //Console.WriteLine(deck.Cards.Count);            
-            //Console.ReadLine();
+            Deck deck = new Deck();
+            deck.Shuffle(3);
+
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
         }
 
        
