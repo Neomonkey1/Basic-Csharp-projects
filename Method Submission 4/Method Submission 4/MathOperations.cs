780559f476cf8ead08888a9acd7379b73d26ef72
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-15 20:32:43 -0400

    Method Submission 4
    created public void class and method, called class and method in program, gave arguments. Added comments

---
+++ Method Submission 4/Method Submission 4/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_4
+{
+    public class MathOperations
+    {
+        //  Above made class public
+        //  Made method public void and to take two parameters
+        public void DivideNumbers(int num1, int num2)
+        {
+            //  Method operation is taking parameter num1 and divide by 3 and print to console,
+            //  also take parameter num2 and print it to console.
+            int results1 = num1 / 3;
+            Console.WriteLine($"The result of {num1} divide by 3 equals: {results1}");
+            Console.WriteLine($"The second input was {num2}");
+        }
+    }
+}
---
+++ Method Submission 4/Method Submission 4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Submission_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Instantiate class 
+            MathOperations math1 = new MathOperations();
+
+            //  Call method from class and give it two numbers
+            math1.DivideNumbers(25, 5);
+
+            //  Call method from class and naming parameters while showing order does not effect output
+            math1.DivideNumbers(num2: 10, num1: 200);
+
+
+            Console.ReadLine();
+        }
+    }
+}
