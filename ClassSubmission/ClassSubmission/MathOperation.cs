bd8923ca103fb6b5e2399059665e8b710b13c4b8
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-15 22:01:42 -0400

    ClassSubmission
    Created class and methods, overloaded one of the methods, got user input converted to int variable, used method with variable, declare class static, Added comments

---
+++ ClassSubmission/ClassSubmission/MathOperation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    public class MathOperation
+    {
+        //  Above made class public
+        //  Method public void, takes 1 parameter, perform operation and print result to console
+        public void DivideBy2(int num1)
+        {
+            int results = num1 / 2;
+            Console.WriteLine($"Result of {num1} divide by 2 equals: {results}");
+        }
+
+        //  Method public void, takes 1 parameter and 1 output parameter, perform operation and gets output parameter
+        public void AddByTen(int num1, out int output )
+        {
+            output = num1 + 10;
+        }
+
+        //  Overload method public void by making parameter a double, perform operation and print result to console.
+        public void AddByTen(double num1)
+        {
+            double result = num1 + 10;
+            Console.WriteLine($"Result from using type double {num1} + 10 equals: {result} ");
+
+        }
+    }
+}
---
+++ ClassSubmission/ClassSubmission/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    //  Made class Program static
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Instantiate Class 
+            MathOperation math1 = new MathOperation();
+
+            //  Asking user for input, convert input to int variable
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //  Calling method and using user input for parameter
+            math1.DivideBy2(num1);
+
+            //  Making output variable. 
+            //  Calling method and using user input for parameter, getting output from method and printing to console the result
+            int output;
+            math1.AddByTen(num1, out output);
+            Console.WriteLine($"Getting result of {num1} + 10 equals: {output}");
+
+            //  Calling overload method, using input double value 
+            math1.AddByTen(15.55);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
