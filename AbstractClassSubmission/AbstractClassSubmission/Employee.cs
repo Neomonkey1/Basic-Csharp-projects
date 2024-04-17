2af04d424f41a10d01ad49b03543f84c6d5069dd
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-16 20:34:04 -0400

    AbstractClassSubmission
    Created abstract class and method, created inherit class and override method, called superclass and initialize object, called method. Added comments

---
+++ AbstractClassSubmission/AbstractClassSubmission/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public class Employee: Person
+    {
+        //  Above superclass inherit from abstract class Person
+        //  Using the abstract method SayName(), override SayName() so class can use method 
+        public override  void SayName()
+        {
+            string FullName = $"{FirstName} {LastName}";
+            Console.WriteLine($"Employee Name: {FullName}");
+        }
+    }
+}
---
+++ AbstractClassSubmission/AbstractClassSubmission/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassSubmission
+{
+    public abstract class Person
+    {
+        //  Above made public abstract class
+        //  Making two properties object
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //  Making abstract method that all inherit class can use.
+        public abstract void SayName();
+                 
+        
+    }
+}
