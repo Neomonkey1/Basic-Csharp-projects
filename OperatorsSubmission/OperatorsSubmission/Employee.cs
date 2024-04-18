ad1f05e839d2ab0d7521a4b6fc3245a7d37d7616
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-17 20:34:36 -0400

    OperatorsSubmission
    Created class and overloaded operators, called class and created two objects, using overloaded operators compared the objects. displayed results to console. Added comments

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    public class Employee
+    {
+        // Made class public, made three properties, one int two string types
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //  Overloading " == " operator to compare Employee objects based on Id
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+
+        //  Overloading " != " operator to compare Employee objects based on Id,
+        //  must overload " == " and " != " as a pair
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1.Id == employee2.Id);
+        }
+    }
+
+    
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Instantiate class and making two Employee objects
+            Employee employee1 = new Employee { Id = 1, FirstName = "John", LastName = "Doe" };
+            Employee employee2 = new Employee { Id = 2, LastName = "Smith", FirstName = "Jane" };
+
+            //  Comparing the two objects using the overloaded operators
+            if (employee1 == employee2)
+            {
+                Console.WriteLine($"{employee1.FirstName} and {employee2.FirstName} have the same ID");
+            }
+            else
+            {
+                Console.WriteLine($"{employee1.FirstName} and {employee2.FirstName} do not have the same ID");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
