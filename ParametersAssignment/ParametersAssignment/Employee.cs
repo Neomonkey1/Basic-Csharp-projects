b7b6a344a563049dda24ade6ee2231ad0e0cd540
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-17 21:18:45 -0400

    ParametersAssignment
    Created generic class with generic type parameter, Called class made object with type string and type int, printed to console. Added comments

---
+++ ParametersAssignment/ParametersAssignment/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    public class Employee<T>  //  Create generic Employee class
+    {
+        //  Creatie property called Things with generic List matching the class
+        public List<T> Things { get; set; }
+    }
+}
---
+++ ParametersAssignment/ParametersAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Instantiate an Employee object with type string as generic parameter
+            Employee<string> employeeString = new Employee<string>();
+            employeeString.Things = new List<string> { "Pencil", "Pen", "Paper", "Ruler" };
+
+            //  Instantiate an Employee object with type int as generic parameter
+            Employee<int> employeeInt = new Employee<int>();
+            employeeInt.Things = new List<int> { 1, 2, 3, 4 };
+
+            //  Using FOREACH loop to display all things from string List
+            Console.WriteLine("Employee objects as \"string\": ");
+            foreach (string thing in employeeString.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            //  Using FOREACH loop to display all things from int List
+            Console.WriteLine("\nEmployee objects as \"int\": ");
+            foreach (int thing in employeeInt.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
