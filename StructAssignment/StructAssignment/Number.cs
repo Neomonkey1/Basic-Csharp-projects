b6a98bababef5d8c89cf046c2b6f82c828b932c1
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-19 20:34:27 -0400

    StructAssignment
    Created a struct, made a object and give it a value, printed to console the value.
    Added comments

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //  Made struct public
+    public struct Number
+    {
+        //  Made property with data type decimal
+       public decimal Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Created object 
+            Number num = new Number();
+
+            //  Give the property Amount a value
+            num.Amount = 25.50m;
+
+            //  Printing to console the object value
+            Console.WriteLine($"Struct number entered is {num.Amount}");
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
