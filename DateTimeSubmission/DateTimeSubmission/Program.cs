13584df2048e17e5fb3e0047a83f35dca5791f74
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-24 21:02:46 -0400

    DateTimeSubmission
    Using DateTime.Now printed to console current time, ask for user input and converted to double variable, using AddHours() with user input to add to current time, printed result to console. Added comments

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Print to console current time
+            Console.WriteLine($"The current time is: {DateTime.Now}");
+
+            //  Asking for user input, convert to double variable
+            Console.WriteLine("Please enter a number.");
+            double userNum = Convert.ToDouble(Console.ReadLine());
+
+            //  Made variable "futureTime" using DateTime.Now for current time and
+            //  using AddHours with user input to add hours to current time to get "futureTime" 
+            DateTime futureTime = DateTime.Now.AddHours(userNum);
+
+            //  Print to console user input and futureTime
+            Console.WriteLine($"The time in {userNum} hours will be {futureTime}");
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
