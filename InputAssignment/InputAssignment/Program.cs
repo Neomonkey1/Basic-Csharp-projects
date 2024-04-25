0cdaec74b94f9159742114a42be46f1f7c08bac5
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-24 20:16:38 -0400

    InputAssignment
    import System.IO, asked for user input, logged user input to .txt file, read .txt file and made variable of .txt file, printed to console. Added comments

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{  //  Above add "using System.IO"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Asking user input, saving input to string variable
+            Console.WriteLine("Please pick a number.");
+            string numText = Console.ReadLine();
+
+            //  Write user input to a .txt file using "using" statement
+            using (StreamWriter file = new StreamWriter(@"C:\Users\neomo\OneDrive\Documents\GitHub\Basic-C--projects\InputAssignment\Log\log.txt", true))
+            {
+                file.WriteLine(numText);
+            }
+
+            //  Using "using" statement to read .txt file, making string variable of .txt file and printing to console.
+            using (StreamReader file1 = new StreamReader(@"C:\Users\neomo\OneDrive\Documents\GitHub\Basic-C--projects\InputAssignment\Log\log.txt"))
+            {
+                string numFromFile = file1.ReadLine();
+                Console.WriteLine($"The number you picked is logged in {numFromFile}");
+            }
+
+            
+
+
+            Console.ReadLine();
+        }
+    }
+}
