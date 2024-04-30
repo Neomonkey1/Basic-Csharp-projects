78f908944ea048bbfec8ce25d4ac065efe4863a7
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-04-29 21:01:27 -0400

    MvcTutorial
    learning basic MVC with C#

---
+++ MvcTutorial/MvcTutorial/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            throw new Exception("Invalid page");
+
+            return View();
+        }
+
+        public ActionResult Contact(int Id=10)
+        {
+            ViewBag.Message = Id;
+
+            return View();
+        }
+    }
+}
