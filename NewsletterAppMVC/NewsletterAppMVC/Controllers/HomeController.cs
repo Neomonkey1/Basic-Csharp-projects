31282bf6cd6a535c30d66ad232400a9b3378b461
Neomonkey1 <150760167+Neomonkey1@users.noreply.github.com>
2024-05-02 20:28:14 -0400

    NewsletterAppMVC
    Added ENTITY framework to app, Updated DataBase, Added "removed" button, added method.

--- NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
+++ NewsletterAppMVC/NewsletterAppMVC/Controllers/HomeController.cs
@@ -11,9 +11,7 @@ using System.Web.Mvc;
 namespace NewsletterAppMVC.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly string connectionString = @"Data Source=MONKEYGAMING\SQLEXPRESS;Initial Catalog=Newsletter;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-
+    {     
         public ActionResult Index()
         {
             return View();
@@ -28,67 +26,20 @@ namespace NewsletterAppMVC.Controllers
             }
             else
             {
-
-                
-
-                string queryString = @"INSERT INTO SignUp (FirstName, LastName, EmailAddress) VALUES
-                                       (@FirstName, @LastName, @EmailAddress)";
-
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                using(NewsletterEntities db = new NewsletterEntities())
                 {
-                    SqlCommand command = new SqlCommand(queryString, connection);
-                    command.Parameters.Add("@FirstName", SqlDbType.VarChar);
-                    command.Parameters.Add("@LastName", SqlDbType.VarChar);
-                    command.Parameters.Add("@EmailAddress", SqlDbType.VarChar);
+                    var signup = new SignUp();
+                    signup.FirstName = firstName;
+                    signup.LastName = lastName;
+                    signup.EmailAddress = emailAddress;
 
-                    command.Parameters["@FirstName"].Value = firstName;
-                    command.Parameters["@LastName"].Value = lastName;
-                    command.Parameters["@EmailAddress"].Value = emailAddress;
-
-                    connection.Open();
-                    command.ExecuteNonQuery();
-                    connection.Close();
-                }
+                    db.SignUps.Add(signup);
+                    db.SaveChanges();
+                }            
 
                 return View("Success");
             }
         }
-
-        public ActionResult Admin()
-        {
-            string queryString = @"SELECT Id, FirstName, LastName, EmailAddress, SocialSecurityNumber from Signup";
-            List<NewsletterSignUp> signups = new List<NewsletterSignUp>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                SqlCommand command = new SqlCommand(queryString, connection);
-
-                connection.Open();
-
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    var signup = new NewsletterSignUp();
-                    signup.Id = Convert.ToInt32(reader["Id"]);
-                    signup.FirstName = reader["FirstName"].ToString();
-                    signup.LastName = reader["LastName"].ToString();
-                    signup.EmailAddress = reader["EmailAddress"].ToString();
-                    signup.SocialsecurityNumber = reader["SocialSecurityNumber"].ToString();
-                    signups.Add(signup);
-                }
-            }
-            var signupVms = new List<SignupVm>();
-            foreach ( var signup in signups)
-            {
-                var signupVm = new SignupVm();
-                signupVm.FirstName = signup.FirstName;
-                signupVm.LastName = signup.LastName;
-                signupVm.EmailAddress = signup.EmailAddress;
-                signupVms.Add(signupVm);
-            }
-
-            return View(signupVms);
-        }
+       
     }
 }
--- NewsletterAppMVC/NewsletterAppMVC/ViewModels/SignupVm.cs
+++ NewsletterAppMVC/NewsletterAppMVC/ViewModels/SignupVm.cs
@@ -7,6 +7,7 @@ namespace NewsletterAppMVC.ViewModels
 {
     public class SignupVm
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
